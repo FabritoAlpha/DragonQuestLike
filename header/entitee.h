396d75f3c93179783d79cfa2c2ed419db57bd090
Beranger Anna <anbberanger@gmail.com>
2022-01-18 11:31:33 +0100

    include dans .h

--- header/entitee.h
+++ header/entitee.h
@@ -6,3 +6,34 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+
+typedef struct{
+  int pv;
+  float x;
+  float y;
+} t_entitee;
+
+typedef struct{
+  t_entitee entitee;
+  int inventaire[10];
+}t_joueur;
+
+typedef struct{
+  t_entitee entitee;
+  int niveau;
+  int en_vie;
+} t_monstre;
+
+typedef struct{
+  int type;
+  float x;
+  float y;
+}t_nonCombattant;
+
+void a_gauche(t_entitee* entitee);
+
+void a_droite(t_entitee* entitee);
+
+void en_bas(t_entitee* entitee);
+
+void en_haut(t_entitee* entitee);
--- header/monde.h
+++ header/monde.h
@@ -7,3 +7,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "entitee.h"
+
+typedef struct{
+
+} t_objet;
+
+typedef struct{
+
+} t_salle;
+
+typedef struct{
+
+} t_zone;
+
+typedef struct{
+
+} t_monde;
