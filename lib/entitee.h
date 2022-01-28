87d687bf8fdd2b5e77d86f873f0b3adaf653e561
Beranger Anna <anbberanger@gmail.com>
2022-01-28 10:18:50 +0100

    structures et signatures de fonction

--- lib/entitee.h
+++ lib/entitee.h
@@ -7,54 +7,77 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define TAILLE_INVENTAIRE 0;
+#define NB_EQUIPEMENT 0;
+
 /**
-  * \struct t_entitee
+  * \struct combattant_t
   * \brief représentation d'une entitee combattante
 */
 typedef struct{
   int pv; /**< nombre de point de vie */
+  int attaque; /**< valeur de ses points d'attaques */
   float x; /**< possition sur l'axe des abscisses */
   float y; /**< position sur l'axe des ordonnées */
-} t_entitee;
+} combattant_t;
 
 /**
-  * \struct t_joueur
+  * \struct objet_t
+  * \brief représentation d'un objet
+*/
+typedef struct{
+  int id; /**< id de l'objet */
+  int attaque_sup; /**< attaque de l'objet */
+  char* nom; /**< nom de l'objet */
+  char* description; /**< description de l'objet */
+} objet_t;
+
+/**
+  * \struct joueur_t
   * \brief représentation du joueur
 */
 typedef struct{
-  t_entitee entitee; /**< points de vie et position */
-  int inventaire[10]; /**< objets en possession du joueur */
-}t_joueur;
+  combattant_t* combattant; /**< points de vie et position */
+  int inventaire[TAILLE_INVENTAIRE]; /**< id des objets en possession du joueur */
+  int objet_equipe[NB_EQUIPEMENT]; /**< id des objets équipés par le joueur */
+} joueur_t;
 
 /**
-  * \struct t_monstre
+  * \struct monstre_t
   * \brief représentation d'un monstre
 */
 typedef struct{
-  t_entitee entitee; /**< points de vie et position */
+  combattant_t* combattant; /**< points de vie et position */
   int niveau; /**< niveau du monstre */
   int en_vie; /**< mort: 0, en vie: 1 */
-} t_monstre;
+} monstre_t;
 
 /**
-  * \struct t_entitee
+  * \struct nonCombattant_t
   * \brief représentation d'une entitee non combattante
 */
 typedef struct{
-  int type; /**< coffre: 0, pnj n°1: 1, pnj n°2: 2 */
+  int id; /**< id des entités coffre: 0, pnj n°1: 1, pnj n°2: 2 */
   float x; /**< possition sur l'axe des abscisses */
   float y; /**< position sur l'axe des ordonnées */
-}t_nonCombattant;
+  int visite; /**< 1: le joueur a déjà intéragi avec l'entité */
+} nonCombattant_t;
 
 /**
-  * \fn void a_gauche(t_entitee* entitee)
+  * \fn void a_gauche(combattant_t*)
   * \param entitee entitée à déplacer
   * \brief déplacement d'une entitée à gauche
 */
-void a_gauche(t_entitee* entitee);
+void a_gauche(combattant_t* entitee);
 
-void a_droite(t_entitee* entitee);
+void a_droite(combattant_t* entitee);
 
-void en_bas(t_entitee* entitee);
+void en_bas(combattant_t* entitee);
 
-void en_haut(t_entitee* entitee);
+void en_haut(combattant_t* entitee);
+
+void init_joueur(joueur_t* joueur, int pv, int attaque, int x, int y);
+
+void init_monstre(monstre_t* monstre, int pv, int attaque, int x, int y, int niveau);
+
+void init_nonCombattant(nonCombattant_t* nonCombattant)
