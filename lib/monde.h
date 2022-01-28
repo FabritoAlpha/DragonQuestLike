0119277ac57adbe34f6e17970e07db146e0907b7
Choux Alex <s196370@ic2s124-07.univ-lemans.fr>
2022-01-28 09:22:42 +0100

    modif header en lib

---
+++ lib/monde.h
@@ -0,0 +1,25 @@
+/**
+  * \file monde.h
+  * \brief header du monde
+  * \author Anna Béranger, Arthur Fabre, Alex Choux
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "entitee.h"
+
+typedef struct{
+
+} t_objet;
+
+typedef struct{
+
+} t_salle;
+
+typedef struct{
+
+} t_zone;
+
+typedef struct{
+
+} t_monde;
