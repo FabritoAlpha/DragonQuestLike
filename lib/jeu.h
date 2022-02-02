d8798adc1d5c2c92ed058143454512990ab6a485
Choux Alex <s196370@ic2s112-01.univ-lemans.fr>
2022-02-02 12:00:36 +0100

    création librairie de jeu

---
+++ lib/jeu.h
@@ -0,0 +1,52 @@
+/**
+  * \file monde.h
+  * \brief header du monde
+  * \author Anna Béranger, Arthur Fabre, Alex Choux
+*/
+
+#ifndef _JEU_H
+#define _JEU_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "monde.h"
+
+
+/**
+	*\struct salle_t
+	*\brief représentation d'une salle
+*/
+
+//On a besoin d'une fonction qui crée le jeu ? -> Une fonction qui l'initialise -> Soit à partir d'un nouveau, soit à partir d'une partie déjà sauvegardee ?
+//Fonction de sauvegarde soit qui écrase la sauvegarde précédente si elle existe, soit une nouvelle sauvegarde si la partie est nouvelle
+//Fonction de fin de partie
+//Fonction de combat ?
+//Fonction qui gère l'affichage de l'inventaire
+//Fonction qui gère l'affichage du menu
+//Fonction qui gère le déplacement du joueur et une celle des monstres
+//Fonction qui gère l'affichage des boutiques
+//Fonction qui gère les dialogues
+
+
+void creation_jeu();
+void init_partie();
+void save_partie();
+void fin_partie();
+void combat();
+void affichage_menu_combat();;
+void affichage_inventaire();
+void affichage_menu();
+void deplacement_personnage();
+void deplacement_monstre();
+void affichage_boutique();
+void affichage_dialogue();
+
+/**
+	*\fn void init_salle(salle_t * salle, int num_salle)
+	*\brief initialisation d'une salle d'une zone
+	*\param salle une salle d'une zone
+	*\param num_salle numéro de la salle de la zone
+*/
+void init_salle(salle_t * salle, int num_salle);
+
+#endif
