f331cdc2f665b41157d8227e8882a1741ce27c46
Fabrito <arthurfabre@orange.fr>
2022-03-11 19:35:53 +0100

    ajout d'un fichier contenant du code pour le calcule de distance

---
+++ code_temp.c
@@ -0,0 +1,13 @@
+//Fichier temportaire pour les fonction de déplacement du monstre
+// Le but étant de ne pas entrer en conflit avec vos push le temps d'avoir une version stable.
+// et étant donné le fait que je ne peux pas vérifier que mon code fonctionne à cause de l'érreur d'initialisation de la sdl.
+
+// La distance sur un repère orthonormé ce calcule grance à la formule suivante:
+// AB = sqrt(pow(xb-xa)+pow(yb-ya))
+// le problème c'est que nous ne nous situons pas dans un repère orthonormé classique
+// je travail donc sur la rédaction d'une formule qui fonctionne avec le repère de la sdl
+// d'apres des forums la formule fonctionnerai.
+
+int distance(combattant_t * joueur,combattant_t * monstre){// renvoie la distance entre deux combattants.
+//
+}
